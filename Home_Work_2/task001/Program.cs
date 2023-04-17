fd8539bf304d5abe29d548567a40ebae14fecd7f
ivan_razuvaev <alfredvilgelm@gmail.com>
2023-04-17 13:14:42 +0300

    task001

---
+++ Home_Work_2/task001/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Написать программу, которая принимает на вход
+трехзначное число и на выходе показывает вторую 
+цифру этого числа
+*/
+Console.Clear();
+//Input
+Console.Write("Введите любое трёхзначное число: ");
+string input = Console.ReadLine() ?? "";
+//Logic
+var a = int.Parse(input);
+var b = a % 100;
+var c = b / 10;
+Console.Write($"{a}->{c}");
