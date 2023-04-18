9b8988bc84512ced1ec99475341e7da195c7c368
ivan_razuvaev <alfredvilgelm@gmail.com>
2023-04-18 17:37:29 +0300

    added HomeWork2

---
+++ Home_Work_2/task002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+//input
+Console.WriteLine("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+//Logic
+var a = int.Parse(userInput);
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры числа не существует!");
+}
+else if (a > 999)
+{
+    var b = a / 10 % 10;
+    Console.Write($"Третья цифра числа -> {b}");
+}
+else if (a>10000)
+{
+    var b1 = a / 100 % 100;
+    Console.Write($"Третья цифра заданного числа ={b1}");
+}
---
+++ Home_Work_2/task003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели,
+// и проверяет, является ли этот день выходным
+
+//Input
+Console.Write("Введите номер дня недели ");
+string nubmerDay = Console.ReadLine() ?? "";
+
+//Logic
+int weekDay = int.Parse(nubmerDay);
+
+if (weekDay <= 5 && weekDay == 1 && weekDay == 2 && weekDay == 3 && weekDay==4);
+    Console.Write("Будний день");
+if (weekDay > 5 && weekDay == 6)
+    Console.Write("УРА! Суббота, выходной день");
+if (weekDay > 5 && weekDay == 7)
+    Console.Write("УРА! Воскресенье, выходной день");
+if (weekDay > 7)
+    Console.Write("Такого дня недели не существует");
